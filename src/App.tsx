298ccba4d47901b2fbde7e4afcb2bf11c56e9dd6
ROMAIN LEMONNIER <lemonnier.romain13@gmail.com>
2024-10-23 17:05:37 +0200

    ajout props 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-// import "./App.css";
 import PokemonCard from "./components/PokemonCard";
-
+const pokemonList = [
+  {
+    name: "Machopeur !",
+    imgSrc: "https://www.pokepedia.fr/images/7/74/Machopeur-PDMC.png",
+  },
+  {
+    name: "mew",
+  },
+];
 function App() {
   return (
-    <>
-      <di>
-        <PokemonCard />
-      </di>
-    </>
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
   );
 }
-
 export default App;
